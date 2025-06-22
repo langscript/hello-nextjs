281a7b19ddf042f41913174c395a7bfff311041f
zhoudalang <65067882+zhoudalang@users.noreply.github.com>
2025-06-22 22:10:07 +0800

    Add create,update and delete feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard'
+  },
+  description: 'A Next.js Demo',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
 
 export default function RootLayout({
   children,
